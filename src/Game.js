c3842ee75734d4f0d076cb8f7144448a9c6e5fc4
Nicolas114 <gonzaleznicolas574@gmail.com>
2021-05-10 14:30:47 -0300

    Añadidos los efectos de cambio de paintmode

--- src/Game.js
+++ src/Game.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import PengineClient from './PengineClient';
-import Board from './Board';
-import Mode from './Mode';
+import React from "react";
+import PengineClient from "./PengineClient";
+import Board from "./Board";
+import Mode from "./Mode";
 
 class Game extends React.Component {
-
   pengine;
 
   constructor(props) {
     super(props);
     this.state = {
       grid: null,
-      mode: '#',
-      waiting: false
+      mode: "#",
+      rowClues: null,
+      colClues: null,
+      waiting: false,
     };
     this.handleClick = this.handleClick.bind(this);
     this.handlePengineCreate = this.handlePengineCreate.bind(this);
@@ -20,11 +21,13 @@ class Game extends React.Component {
   }
 
   handlePengineCreate() {
-    const queryS = 'init(PistasFilas, PistasColumns, Grilla)';
+    const queryS = "init(PistasFilas, PistasColumns, Grilla)";
     this.pengine.query(queryS, (success, response) => {
       if (success) {
         this.setState({
-          grid: response['Grilla']
+          grid: response["Grilla"],
+          rowClues: response["PistasFilas"],
+          colClues: response["PistasColumns"],
         });
       }
     });
@@ -35,23 +38,30 @@ class Game extends React.Component {
     if (this.state.waiting) {
       return;
     }
-
     // Build Prolog query to make the move, which will look as follows:
     // put("#",[0,1],[], [],[["X",_,_,_,_],["X",_,"X",_,_],["X",_,_,_,_],["#","#","#",_,_],[_,_,"#","#","#"]], GrillaRes, FilaSat, ColSat)
     const squaresS = JSON.stringify(this.state.grid).replaceAll('"_"', "_"); // Remove quotes for variables.
-    const queryS = 'put("' + this.state.mode + '", [' + i + ',' + j + ']' + ', [], [],' + squaresS + ', GrillaRes, FilaSat, ColSat)';
+    const queryS =
+      'put("' + this.state.mode + '", [' +
+      i +
+      "," +
+      j +
+      "]" +
+      ", [], []," +
+      squaresS +
+      ", GrillaRes, FilaSat, ColSat)";
     this.setState({
-      waiting: true
+      waiting: true,
     });
     this.pengine.query(queryS, (success, response) => {
       if (success) {
         this.setState({
-          grid: response['GrillaRes'],
-          waiting: false
+          grid: response["GrillaRes"],
+          waiting: false,
         });
       } else {
         this.setState({
-          waiting: false
+          waiting: false,
         });
       }
     });
@@ -61,23 +71,39 @@ class Game extends React.Component {
     if (this.state.grid === null) {
       return null;
     }
-    const statusText = 'Keep playing!';
+    const statusText = "Keep playing!";
     return (
       <div className="game">
         <Board
           grid={this.state.grid}
-          onClick={(i, j) => this.handleClick(i,j)}
+          rowClues={this.state.rowClues}
+          colClues={this.state.colClues}
+          onClick={(i, j) => this.handleClick(i, j)}
         />
-        <div className="gameInfo">
-          {statusText}
-        </div>
+        <div className="gameInfo">{statusText}</div>
         <Mode
-          onClick={(e) => this.setState({
-            mode: e.target.value
-          })}
-        />
+          onClick={(e) => this.handleModeClick(e)}
+          />
       </div>
     );
+  }
+
+  handleModeClick(e) {
+    const divmode = e.target.parentElement;
+    const clickedButton = e.currentTarget;
+
+    this.setState({
+      mode: e.target.value,
+    })
+
+    if (clickedButton.value == '#'){
+      divmode.children[2].style = "background: blue";
+      divmode.children[1].style = "background: null";
+    }
+    else {
+      divmode.children[2].style = "background: null";
+      divmode.children[1].style = "background: blue";
+    }
   }
 }
 
--- src/Mode.js
+++ src/Mode.js
@@ -1,12 +1,18 @@
 import React from "react";
 
 class Mode extends React.Component {
-  render() {
-    return (
+
+    
+    render() {
+        return (
       <div className="mode">
         <p>Mode: </p>
-        <button value="X" onClick={this.props.onClick}>X</button>
-        <button value="#" onClick={this.props.onClick}>#</button>
+        <button value="X" onClick={this.props.onClick}>
+          X
+        </button>
+        <button value="#" onClick={this.props.onClick}>
+          #
+        </button>
       </div>
     );
   }
